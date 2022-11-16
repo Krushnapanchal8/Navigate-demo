182bca0462d3c784a6c7b4bc948cfe47ef32c6a8
Krushnapanchal8 <86007243+Krushnapanchal8@users.noreply.github.com>
2022-11-16 13:29:46 +0530

    Add files via upload

---
+++ navigateDemo/navigateDemo/CalcultorBrain.swift
@@ -0,0 +1,9 @@
+//
+//  CalcultorBrain.swift
+//  BMI Calculator
+//
+//  Created by Mac on 11/11/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
---
+++ navigateDemo/navigateDemo/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  navigateDemo
+//
+//  Created by Mac on 11/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func goToHomePressed(_ sender: UIButton) {
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+}
+
